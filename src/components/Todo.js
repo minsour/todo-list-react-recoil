9961bdb262d0a374a27595e45efa28781d28c5f2
minsoopark <minsoopark@linecorp.com>
2020-09-22 14:32:13 +0900

    Add remove handler to Todo component

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,10 +1,12 @@
 import React, { useCallback } from 'react'
-import { useRecoilState, useSetRecoilState } from 'recoil'
-import { clickedTodoState, todoState } from '../store/todoStore'
+import { useRecoilState, useSetRecoilState, useResetRecoilState } from 'recoil'
+import { todoListState, clickedTodoState, todoState } from '../store/todoStore'
 
 const Todo = ({ id }) => {
   const [todo, setTodo] = useRecoilState(todoState(id))
+  const setTodoList = useSetRecoilState(todoListState)
   const setClickedTodo = useSetRecoilState(clickedTodoState)
+  const resetTodo = useResetRecoilState(todoState(id))
 
   const handleToggleCompletion = useCallback(() => {
     setTodo(prevTodo => ({
@@ -15,6 +17,14 @@ const Todo = ({ id }) => {
 
   const handleClickTodo = useCallback(() => {
     setClickedTodo(id)
+  }, [])
+
+  const handleRemoveTodo = useCallback(() => {
+    setTodoList(prevTodos => [
+      ...prevTodos.slice(0, id - 1),
+      ...prevTodos.slice(id)
+    ])
+    resetTodo()
   }, [])
 
   return (
@@ -28,7 +38,9 @@ const Todo = ({ id }) => {
       <div className='todoTitle' onClick={handleClickTodo}>
         {todo.title}
       </div>
-      <button className='todoRemoveButton'>X</button>
+      <button className='todoRemoveButton' onClick={handleRemoveTodo}>
+        X
+      </button>
     </li>
   )
 }
